a29936e4078cceb6689525a9c2080af8d9c8b9ce
Ramon van Heerde <ramonvheerde@hotmail.com>
2023-03-07 09:13:06 +0100

    starter-template

--- src/App.js
+++ src/App.js
@@ -1,49 +1,20 @@
-import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [items, setItems] = useState([]);
-  const [text, setText] = useState("");
-
-  function handleChange(e) {
-    setText(e.target.value);
-  }
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    setItems([...items, text]);
-    setText("");
-  }
-
-  function handleRemove(index) {
-    const newItems = [...items];
-    newItems.splice(index, 1);
-    setItems(newItems);
-  }
-
   return (
     <div className='shopping-list'>
       <h2>Boodschappenlijstje</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type='text'
-          value={text}
-          onChange={handleChange}
-          placeholder='Item toevoegen'
-        />
+      <form>
+        <input type='text' placeholder='Item toevoegen' />
         <button className='add' type='submit'>
           Toevoegen
         </button>
       </form>
       <ul>
-        {items.map((item, index) => (
-          <li key={index}>
-            <span>{item}</span>
-            <button className='remove' onClick={() => handleRemove(index)}>
-              Verwijder
-            </button>
-          </li>
-        ))}
+        <li>
+          <span>Appels</span>
+          <button className='remove'>Verwijder</button>
+        </li>
       </ul>
     </div>
   );
